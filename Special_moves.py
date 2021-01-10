659c613d8bce5a7214d4be390da3bff8cd0c2e36
jyw2 <jyw2@ualberta.ca>
2021-01-09 21:45:56 -0700

    Cmd Class added, base documents added

---
+++ Special_moves.py
@@ -0,0 +1,25 @@
+class CMD:
+    """Class that holds the CMD pattern to search for in the input stream
+    and the CMD priority. Each Special move has an associated CMD class"""
+    def __init__(self, targetCmd, priority):
+       self.targetCmd = targetCmd
+       self.priority = priority
+
+    def get_targetCmd():
+        return self.targetCmd
+
+    def get_priority()
+        return self.priority
+
+
+class Input_stream:
+    """holds the inputs in memory for reading.
+    a null value means no input"""
+
+    def __init__():
+        self.stream = [null]*10
+        
+    def add_input(input):
+
+
+    
